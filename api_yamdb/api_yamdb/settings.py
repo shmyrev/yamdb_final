a1a31c86b0ce4f79983f6a9fec2bcb3a9c1361e8
Shmyrev <sh.diman@gmail.com>
2023-02-15 09:16:46 +0300

    053

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('TOKEN', default='54321')
 
-DEBUG = False
+DEBUG = os.getenv('DEBUG', default='False')
 
 ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS', default='*')]
 
